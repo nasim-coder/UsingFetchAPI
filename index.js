47328557923c375ffa20815af71ab44a9f1e1503
Nasim Ahamad Khan <40820072+NakRex@users.noreply.github.com>
2021-08-27 14:48:47 +0530

    updated

---
+++ index.js
@@ -0,0 +1,11 @@
+// const jokeElenet = document.getElementById("joke");
+// const btnjoke = document.getElementById("jokebtn");
+
+
+// btnjoke.addEventListener("click", jokeGenerator);
+
+function jokeGenerator(){
+    const jokes = fetch("https://api.jokes.one");
+
+}
+jokeGenerator();
