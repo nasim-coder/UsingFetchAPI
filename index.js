13741cbe441c77452b0ada1d26d5366b0e50cca8
Nasim Ahamad Khan <40820072+NakRex@users.noreply.github.com>
2021-08-29 21:31:56 +0530

    updated

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-const jokeElenet = document.getElementById("joke");
+const jokeElemenet = document.getElementById("joke");
 
-
-function jokeGenerator(){
-    const jokesResponse = fetch("https://api.jokes.one", {
-        mode: 'no-cors',
-        method: "post",
-        headers: {
-             "Content-Type": "application/json"
-        }
-    })
-    
-    const jokes =  jokesResponse.json;
-    console.log(jokes);
-}
+function jokeGenerator() {
+    // make an API request to https://icanhazdadjoke.com/'
+    fetch('https://icanhazdadjoke.com/', {
+      headers: {
+        'Accept': 'application/json'
+      }
+    }).then(function(response) {
+      /* convert Stringified JSON response to Javascript Object */
+      return response.json();
+    }).then(function(data) {
+      const joke = data.joke;
+      console.log(joke);
+      jokeElemenet.innerHTML = joke;
+  console.log(joke);  
+    }).catch(function(error) {
+      console.log(error);
+    });
+  }
