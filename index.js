8b947581851cdc3f8d9aeb5012b7a9ce99a79efd
Nasim Ahamad Khan <40820072+NakRex@users.noreply.github.com>
2021-09-13 21:49:52 +0530

    updated

--- index.js
+++ index.js
@@ -1,22 +1,35 @@
 const jokeElemenet = document.getElementById("joke");
 
 function jokeGenerator() {
-    // make an API request to https://icanhazdadjoke.com/'
-    fetch("https://icanhazdadjoke.com/", {
-        headers: {
-            Accept: "application/json",
-        },
+  // make an API request to https://icanhazdadjoke.com/'
+  fetch("https://icanhazdadjoke.com/", {
+    headers: {
+      Accept: "application/json",
+    },
+  })
+    .then(function (response) {
+      /* convert Stringified JSON response to Javascript Object */
+      return response.json();
     })
-        .then(function (response) {
-            /* convert Stringified JSON response to Javascript Object */
-            return response.json();
-        })
-        .then(function (data) {
-            const joke = data.joke;
-            jokeElemenet.innerHTML = joke;
-            console.log(joke);
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
+    .then(function (data) {
+      const joke = data.joke;
+      jokeElemenet.innerHTML = joke;
+      // console.log(joke);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 }
+
+fetch("https://jsonplaceholder.typicode.com/todos")
+  .then(function (response) {
+    //   console.log(response.json());
+    return response.json();
+  })
+  .then(function (data) {
+      for (const key in data) {
+          console.log(data[key].title, data[key].completed);
+      }
+  }).catch(function (err) {
+      log.error(err);
+  });
